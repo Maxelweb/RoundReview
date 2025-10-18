a03fc2ea557666f270462b27669cb70958304099
Maxelweb <marianosciacco@gmail.com>
2025-10-19 00:27:12 +0200

    feat: enrich db operations during initialization

--- app/database.py
+++ app/database.py
@@ -27,8 +27,10 @@ class Database:
         self._cursor = self._client.cursor()
     
     def initialize(self) -> None:
+        """ Init the database operations """
+
         if self.__update_db_schema_version():
-            log.info("Database schema version initialized / updated correctly!")
+            log.info("Database schema version updated correctly!")
         else:
             log.fatal("Unable to update schema in the database. Please check the logs.")
             exit(1)
@@ -37,13 +39,15 @@ class Database:
             self.__create_user_system()
             self.log(USER_SYSTEM_ID, f"user add (user_id={USER_SYSTEM_ID})")
         else: 
-            log.info("System user already created")
+            log.info("System user: already created!")
+
+        self.__check_system_properties()
 
         if self.c.execute("SELECT name FROM user WHERE admin = 1").fetchone() is None:
             self.__create_user_admin()
             self.log(USER_SYSTEM_ID, f"user add (user_id={(USER_SYSTEM_ID+1)})")
         else: 
-            log.info("Admin user already created")
+            log.info("Admin user: at least one already created!")
 
     def close(self) -> None:
         self._client.close()
@@ -57,9 +61,11 @@ class Database:
 
     @staticmethod
     def hash(password:str) -> str:
+        """ Hashing function """
         return hashlib.sha512(password.encode("utf-8")).hexdigest()
 
     def log(self, user_id:int, action:str) -> None:
+        """ Add a log into the database """
         log.info(f"USER_ID {user_id} -> {action}")
         self.c.execute(
             'INSERT INTO log (date, user_id, action) VALUES (?, ?, ?);', 
@@ -70,6 +76,7 @@ class Database:
         self.commit()
 
     def __create_user_system(self) -> None:
+        """ Add the system user that handles automatic operations """
         # Add system user
         self.c.execute(
             'INSERT INTO user (name, email, password, admin) VALUES (?, ?, ?, ?);', 
@@ -79,16 +86,25 @@ class Database:
             -1)
         )
         self.commit()
-        # Add default properties for system user
-        self.c.executemany(
-            'INSERT INTO user_property (key, value, user_id) VALUES (?, ?, ?);', [
-                ("PROJECT_CREATE_DISABLED", "FALSE", USER_SYSTEM_ID),
-                ("OBJECT_DELETE_DISABLED", "FALSE", USER_SYSTEM_ID),
-                ("USER_LOGIN_DISABLED", "FALSE", USER_SYSTEM_ID),
-                ("OBJECT_MAX_UPLOAD_SIZE_MB", SYSTEM_MAX_UPLOAD_SIZE_MB, USER_SYSTEM_ID),
-                ("WEBHOOKS_DISABLED", "FALSE", USER_SYSTEM_ID)
-            ])
-        self.commit()
+
+    def __check_system_properties(self) -> None:
+        """ Add the system properties with defaults (if missing) """
+        expected_props = [
+            ("PROJECT_CREATE_DISABLED", "FALSE"),
+            ("OBJECT_DELETE_DISABLED", "FALSE"),
+            ("USER_LOGIN_DISABLED", "FALSE"),
+            ("OBJECT_MAX_UPLOAD_SIZE_MB", SYSTEM_MAX_UPLOAD_SIZE_MB),
+            ("WEBHOOKS_DISABLED", "FALSE")
+        ]
+        existing_props = {row[0] for row in self.c.execute("SELECT key FROM user_property WHERE user_id = ?;", (USER_SYSTEM_ID,)).fetchall()}
+        props_to_add = [(k, v, USER_SYSTEM_ID) for k, v in expected_props if k not in existing_props]
+
+        if props_to_add:
+            self.c.executemany('INSERT INTO user_property (key, value, user_id) VALUES (?, ?, ?);', props_to_add)
+            self.commit()
+            log.info("System properties: added missing properties for System User: %s", ", ".join([t[0] for t in props_to_add]))
+        else:
+            log.info("System properties: all required properties already created!")
 
     def __create_user_admin(self) -> None:
         self.c.execute(
@@ -99,7 +115,9 @@ class Database:
             1)
         )
         self.commit()
-        log.warning("Default admin user %s created with password: %s", USER_ADMIN_EMAIL, USER_DEFAULT_PASSWORD)
+        log.warning('*' * 30)
+        log.warning("(!) Admin user: adding default user %s with password: %s", USER_ADMIN_EMAIL, USER_DEFAULT_PASSWORD)
+        log.warning('*' * 30)
 
     def __update_db_schema_version(self) -> bool:
 
@@ -136,12 +154,12 @@ class Database:
         
         # Evaluate action based on the db version and the schema version
         if current_version == latest_version:
-            log.info("Database schema up-to-date (version %s)", current_version)
+            log.info("Database schema up-to-date (v%s)", current_version)
         elif current_version > latest_version:
-            log.warning("Database version (%s) has a higher schema version than the available (%s). Something is wrong. Please, make sure you are using the latest version of the application.", current_version, latest_version)
+            log.warning("Database schema (v%s) has a higher schema version than the available (v%s). Something is wrong. Please, make sure you are using the latest version of the application.", current_version, latest_version)
             return False
         elif current_version < latest_version:
-            log.warning("Database version ID=%s is older than latest schema ID=%s, applying updates...", current_version, latest_version)
+            log.warning("Database schema in use (v%s) is older than latest schema (v%s), applying updates...", current_version, latest_version)
             for ver, path in schemas:
                 if ver <= current_version:
                     continue
@@ -150,6 +168,8 @@ class Database:
                     self.c.executescript(sql)
                     self.commit()
                     log.info("Database: applied schema %s", path.name)
+                    if ver > 0: # Minimum schema version for logging
+                        self.log(USER_SYSTEM_ID, f"database schema version update (id={ver})")
                 except Error as e:
                     log.fatal("Database: failed to apply schema %s: %s", path.name, e)
                     return False
@@ -163,7 +183,6 @@ class Database:
             return False
         
         tables = [table[0] for table in raw_tables]
-        log.debug("Checking tables creation...")
         if not all(t in tables for t in required_tables):
             log.warning("Failed to find the following required tables: %s", ", ".join([t for t in required_tables if t not in tables]))
             return False
